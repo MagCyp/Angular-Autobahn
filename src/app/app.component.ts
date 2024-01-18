0817b550c9bcc942233d458782e1cab489272915
Invs_Cypher <dimares53@gmail.com>
2024-01-18 02:16:47 +0100

    load roads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataService } from './services/data.service';
+import { MatListModule } from '@angular/material/list';
+import {
+  MatTabChangeEvent,
+  MatTabGroup,
+  MatTabsModule,
+} from '@angular/material/tabs';
+import { DetailsTableComponent } from './details-table/details-table.component';
+import { DetailsListComponent } from './details-list/details-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatListModule, MatTabsModule, DetailsTableComponent, DetailsListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Angular';
+  @ViewChild(MatTabGroup) tabGroup!: MatTabGroup;
+
+  roads: string[] = [];
+  activeRoad: string = '';
+  activeTab: string = '';
+
+  constructor(
+    private dataService: DataService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.dataService
+      .getData<{ roads: string[] }>('https://verkehr.autobahn.de/o/autobahn/')
+      .subscribe(
+        (responseData: { roads: string[] }) => {
+          this.roads = responseData.roads;
+          this.activeRoad = responseData.roads[0];
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
+
+  ngAfterViewInit(): void {
+    const activeTabIndex = this.tabGroup.selectedIndex!;
+
+    this.activeTab = this.tabGroup._tabs.toArray()[activeTabIndex].ariaLabel;
+    this.cdr.detectChanges();
+  }
+
+  onTabChange($event: MatTabChangeEvent): void {
+    this.activeTab = $event.tab.ariaLabel;
+  }
+
+  setElement(road: string): void {
+    this.activeRoad = road;
+    console.log(this.activeRoad);
+  }
 }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  getData<T>(url: string): Observable<T> {
+    return this.http.get<T>(url);
+  }
+}
